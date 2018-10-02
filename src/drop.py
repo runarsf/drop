5d8eef2325cf29159a9277943399895fb56d68c4
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2018-10-02 14:27:28 +0200

    Added display arg

--- src/drop.py
+++ src/drop.py
@@ -17,33 +17,61 @@ def verify():
     finally:
         import config as c
         if not os.path.exists(c.filepath):
-        	try:
-        		print("Creating file directory")
-        		os.makedirs(c.filepath)
-        	except:
-        		print("Could not create directory. Check config formatting.\nExiting...")
-        		raise SystemExit
+            try:
+                print("Creating file directory")
+                os.makedirs(c.filepath)
+            except:
+                print("Could not create directory. Check config formatting.\nExiting...")
+                raise SystemExit
         if not os.path.isfile(c.filepath+c.filename):
-        	try:
-        		print("Creating file")
-        		file = open(c.filepath+c.filename, 'w+')
-        		file.close()
-        	except:
-        		print("Could not create file. Check config formatting.\nExiting...")
-        		raise SystemExit
+            try:
+                print("Creating file")
+                file = open(c.filepath+c.filename, 'w+')
+                file.close()
+            except:
+                print("Could not create file. Check config formatting.\nExiting...")
+                raise SystemExit
 verify()
+import config as c
 
 def helpme():
     print('\n\tUsage: python3 drop.py [arguments]\n')
+    print('\t       *           No argument, shows list contents.')
+    print('\t       -l          Same as no argument, Lists out list content.')
     print('\t       -h          Shows this dialog.')
     print('\t       -c          Check/uncheck list.')
-    print('\t       -l List     Sets which todo list to use.')
+    print('\t       -s List     Sets which todo list to use.')
     print('\t                   Creates it if not existing.')
     print('\t       -a Content  Adds an entry to your todo list.')
-    print('\t       -r Index    Removes entry from your todo list.')
+    print('\t       -r Index    Removes an entry from your todo list.')
     print('')
 
 def add():
+    try:
+        fileAdd = open(c.filepath+c.filename, 'a+')
+        fileAdd.write('\n-[]'+sys.argv[2])
+        fileAdd.close()
+        #print(c.filepath+c.filename)
+        #print('-[]'+sys.argv[2])
+    except:
+        print("Could not add entry to list.\nExiting...\n")
+        raise SystemExit
+
+def display():
+    try:
+        print('')
+        fileRead = open(c.filepath+c.filename, 'r')
+        i = 0
+        for line in fileRead:
+            i += 1
+            print(str("{}   ".format(i)+line.strip("\n")))
+        fileRead.close()
+        print('')
+    except:
+        print("Could not display list contents.\nExiting...\n")
+        raise SystemExit
+
+def check():
     pass
 
 def remove():
@@ -52,23 +80,19 @@ def remove():
 def setList():
     pass
 
-def check():
-    pass
-
 if len(sys.argv) > 3:
     print('Too many arguments!')
     raise SystemExit
-elif len(sys.argv) <= 1:
-    print('Too few arguments!')
-    raise SystemExit
 
-if '-h' in sys.argv:
+if len(sys.argv) <= 1 or '-l' in sys.argv:
+    display()
+elif '-h' in sys.argv:
     helpme()
 elif '-a' in sys.argv:
     add()
 elif '-r' in sys.argv:
     remove()
-elif '-l' in sys.argv:
+elif '-s' in sys.argv:
     setList()
 elif '-c' in sys.argv:
     check()
