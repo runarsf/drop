d9821d1a913001bcfae160a51a1db649aa5b1f5f
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2018-10-04 16:35:25 +0200

    Check

--- src/drop.py
+++ src/drop.py
@@ -42,8 +42,11 @@ def helpme():
     print('\t       -h          Shows this dialog.')
     print('\t       -c          Check/uncheck list.')
     print('\t       -s List     Sets which todo list to use.')
+    print('\t       -f Path     Sets list dir (folder)')
     print('\t       -a Content  Adds an entry to your todo list.')
     print('\t       -r Index    Removes an entry from your todo list.')
+    print('\t       -p          Purge completed tasks. Preserves them in ./finishedTasks')
+    print('\t       -d          Delete all completed tasks.')
     print('')
 
 def add():
@@ -57,6 +60,16 @@ def add():
 
 def display():
     try:
+        f = open(c.filepath+c.filename, 'r')
+        l = [l for l in f.readlines() if l.strip()]
+        f.close()
+        f = open(c.filepath+c.filename, 'w+')
+        f.writelines( l )
+        f.close()
+    except:
+        print("Could not remove whitespace from list contents.\nExiting...\n")
+        raise SystemExit
+    finally:
         print('')
         file = open(c.filepath+c.filename, 'r')
         i = 0
@@ -65,27 +78,47 @@ def display():
             print(str("{}   ".format(i)+line.strip("\n")))
         file.close()
         print('')
-    except:
-        print("Could not display list contents.\nExiting...\n")
-        raise SystemExit
 
 def check():
-    file = open(c.filepath+c.filename, 'r')
-    i = 0
-    for line in file:
-        i += 1
-        if i == int(sys.argv[2]):
-            preserveLine = line
-            print(preserveLine)
-    file.close()
-    setline = preserveLine.replace("-[]", "-[x]")
-    print(setline)
-    subprocess.call(["sed", "-i", "-e", "s/asd//g", c.filepath+c.filename])
+    with open(c.filepath+c.filename, 'r') as file:
+        data = file.readlines()
+        if data[int(sys.argv[2])-1][:3] == "-[]":
+            data[int(sys.argv[2])-1] = data[int(sys.argv[2])-1].replace("-[]", "-[x]")
+        else:
+            data[int(sys.argv[2])-1] = data[int(sys.argv[2])-1].replace("-[x]", "-[]")
+        with open(c.filepath+c.filename, 'w') as file:
+            file.writelines( data )
+
 
 def remove():
-    pass
+    with open(c.filepath+c.filename, 'r') as file:
+        data = file.readlines()
+        data[int(sys.argv[2])-1] = ""
+        with open(c.filepath+c.filename, 'w') as file:
+            file.writelines( data )
 
 def setList():
+    print('')
+        file = open('config.py', 'r')
+        i = 0
+        for line in file:
+            i += 1
+            if "filename" in line:
+                with open(c.filepath+c.filename, 'r') as file:
+                    data = file.readlines()
+                    data[int(sys.argv[2])-1] = ""
+                    with open(c.filepath+c.filename, 'w') as file:
+                        file.writelines( data )
+            
+        file.close()
+        print('')
+
+def setFolder():
+
+def purge():
+    pass
+
+def delete():
     pass
 
 if len(sys.argv) > 3:
@@ -104,9 +137,15 @@ elif '-r' in sys.argv:
     remove()
 elif '-s' in sys.argv:
     setList()
+elif '-f' in sys.argv:
+    setFolder()
 elif '-c' in sys.argv:
     check()
     #raise SystemExit # prevent display from being ran twice, see bottom of file
+elif '-p' in sys.argv:
+    purge()
+elif '-d' in sys.argv:
+    delete()
 else:
     print('Unknown operation: {}'.format(sys.argv))
 
